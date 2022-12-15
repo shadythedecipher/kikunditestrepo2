bf7ab6f3d15b9780a08030dd1b1c3d20d035127f
shadrack <shadrack.abel321@gmail.com>
2022-12-15 10:34:45 +0300

    Initial commit

---
+++ src/main/java/com/mingati/kikunditestrepo/KikunditestrepoApplication.java
@@ -0,0 +1,13 @@
+package com.mingati.kikunditestrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KikunditestrepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KikunditestrepoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mingati/kikunditestrepo/KikunditestrepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mingati.kikunditestrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KikunditestrepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
